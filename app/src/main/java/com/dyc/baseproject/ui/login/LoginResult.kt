999b442d27d347610bcd139e875ba9dedb3086de
yancheng <yanchengdeng@gmail.com>
2020-07-30 17:26:08 +0800

    commit
    first commit

---
+++ app/src/main/java/com/dyc/baseproject/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.dyc.baseproject.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
